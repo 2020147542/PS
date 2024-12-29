3d2b8c69f99962abab8209a33e6fa8365a11abec
2020147542 <tmfal5023@yonsei.ac.kr>
2024-12-29 14:03:08 +0900

    12851번 숨바꼭질2 - 성공

--- 20241228/main.cpp
+++ 20241228/main.cpp
@@ -10,7 +10,7 @@
 
 - 입력: 수빈이가 있는 위치 N, 동생이 있는 위치 K
 - 출력: 첫째 줄) 수빈이가 동생을 찾는 가장 빠른 시간
-				둘째 줄) 가장 빠른 시간으로 수빈이가 동생을 찾는 방법의 수
+		둘째 줄) 가장 빠른 시간으로 수빈이가 동생을 찾는 방법의 수
 
 */
 
@@ -21,49 +21,65 @@
 using namespace std;
 
 int N, K;
-int dr[3] = { 1, 1, 2 };
-int dx[3] = { -1, 1, 0 };
+int dr[3] = {1, 1, 2};
+int dx[3] = {-1, 1, 0};
 
 const int maxLength = 100001;
+pair<int, int> visited[maxLength];
 
-pair<int, int> solution(int start_node){
+pair<int, int> solution(int start_node)
+{
 
 	int fastestLevel = 0;
 	int fastestWays = 0;
-	queue<pair<int, int>> q;
-	
-	q.push({fastestLevel, start_node});
-	
-	while(!q.empty()){
-		int curLevel = q.front().first;
-		int curNode = q.front().second;
+	queue<int> q;
+
+	visited[start_node] = {fastestLevel, fastestWays + 1};
+	q.push(start_node);
+
+	while (!q.empty())
+	{
+		int curNode = q.front();
+		int curLevel = visited[curNode].first;
+		int curCount = visited[curNode].second;
 		q.pop();
-		
-		if(curNode == K){
+
+		if (curNode == K)
+		{
 			fastestLevel = fastestWays == 0 ? curLevel : fastestLevel;
-            fastestWays++;
+			fastestWays += curCount;
 		}
 
-        if (fastestLevel && fastestLevel < curLevel) break;
-		
-		for(int i = 0; i < 3; i++){
+		if (fastestLevel && fastestLevel < curLevel) break;
+
+		for (int i = 0; i < 3; i++)
+		{
 			int nextNode = dr[i] * curNode + dx[i];
-			if(nextNode >= 0 && nextNode < maxLength){
-				q.push({curLevel + 1, nextNode});
+			if (nextNode >= 0 && nextNode < maxLength)
+			{
+				if (visited[nextNode].first && visited[nextNode].first == curLevel + 1) {
+					visited[nextNode].second += curCount;
+				}
+				else if(visited[nextNode].first == 0 && nextNode != start_node) {
+					visited[nextNode].first = curLevel + 1;
+					visited[nextNode].second += curCount;
+					q.push(nextNode);
+				}
 			}
 		}
 	}
-	
+
 	return {fastestLevel, fastestWays};
 }
 
-int main(){
-	
+int main()
+{
+
 	cin >> N >> K;
-	
+
 	pair<int, int> result = solution(N);
-    cout << result.first << endl;
+	cout << result.first << endl;
 	cout << result.second << endl;
-	
+
 	return 0;
 }
