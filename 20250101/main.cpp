aca46e6b0be0495b15d535c123045e36f2d7e6ad
2020147542 <tmfal5023@yonsei.ac.kr>
2025-01-01 21:30:48 +0900

    2579번 계단 오르기 > 점화식 답안 참고

--- 20250101/main.cpp
+++ 20250101/main.cpp
@@ -19,47 +19,57 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <algorithm>
 
 using namespace std;
 
 int N;
-vector<long> stairs;
+vector<int> stairs;
 
-void findMax(vector<pair<long, long>> &dp)
-{
-    int maxIdx = 0;
-    for (int i = 3; i <= N; i++)
-    {
-        if (dp[i - 1].second != i - 2 && dp[i - 1].first > dp[i - 2].first)
-            maxIdx = i - 1;
-        else
-            maxIdx = i - 2;
+/*
+    ** 점화식 찾기
+    dp = 현재 계단을 반드시 밟으면서 얻을 수 있는 최대 스코어
+    최대 점수를 가지면서, 1번 계단을 반드시 밟는 경우 : 1
+    최대 점수를 가지면서, 2번 계단을 반드시 밟는 경우 : 1-2
+    최대 점수를 가지면서, 3번 계단을 반드시 밟는 경우 : 1-3, 2-3
+    최대 점수를 가지면서, 4번 계단을 반드시 밟는 경우 : 1-2-4, 1-3-4
+    최대 점수를 가지면서, 5번 계단을 반드시 밟는 경우 : 1-2-4-5, 1-3-5, 2-3-5
 
-        dp[i].first = dp[maxIdx].first + stairs[i];
-        dp[i].second = maxIdx;
+    dp[1] = stair[1]
+    dp[2] = stair[1] + stair[2]
+    dp[3] = max(stair[1] + stair[3], stair[2] + stair[3])
+
+    dp[4] = max(dp[2] + stair[4], dp[1] + stair[3] + stair[4])
+    dp[5] = max(dp[3] + stair[5], dp[2] + stair[4] + stair[5])
+
+    정리하면 dp[N] = max(dp[N-2] + stair[N], dp[N-3] + stair[N-1] + stair[N])
+*/
+int findMax(){
+    vector<int> dp(N + 1, 0);
+    dp[1] = stairs[1];
+    dp[2] = stairs[1] + stairs[2];
+    dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
+
+    for (int i = 4; i <= N; i++){
+        dp[i] = max((dp[i - 2] + stairs[i]), (dp[i - 3] + stairs[i - 1] + stairs[i]));
     }
+
+    return dp[N];
 }
 
 int main()
 {
-
+    // 1. 입력받기
     cin >> N;
 
     stairs.resize(N + 1, 0);
 
-    for (int i = 1; i <= N; i++)
-    {
+    for (int i = 1; i <= N; i++) {
         cin >> stairs[i];
     }
 
-    // 현재 계단까지의 포인트 최댓값 구하기
-    vector<pair<long, long>> dp(N + 1, {0, 0});
-    dp[1] = {stairs[1], 0};
-    dp[2] = {stairs[1] + stairs[2], 1};
-
-    findMax(dp);
-
-    cout << dp[N].first << endl;
+    // 2. 최대값 찾기
+    cout << findMax() << endl;
 
     return 0;
 }
