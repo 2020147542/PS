02744f526a8f8998b5250d93f9425745f85593ec
2020147542 <tmfal5023@yonsei.ac.kr>
2025-01-07 23:49:29 +0900

    부분 수열의 합 > 부분수열 - 재귀로 가능

--- 20250107/main.cpp
+++ 20250107/main.cpp
@@ -6,19 +6,15 @@ using namespace std;
 int N, S, total;
 vector<int> arr;
 
-int solution(int s_idx) {
-  int sum = 0;
+void solution(int s_idx, int sum) {
+  if (s_idx == N) {
+    if (sum == S) total++;
 
-  for (int i = s_idx; i < N; i++) {
-    sum += arr[i];
-
-    if (sum == S) {
-      total++;
-      return i;
-    }
+    return;
   }
 
-  return -1;
+  solution(s_idx + 1, sum + arr[s_idx]);
+  solution(s_idx + 1, sum);
 }
 
 int main() {
@@ -29,11 +25,10 @@ int main() {
     cin >> arr[i];
   }
 
-  for (int i = 0; i < N; i++) {
-    int newBegin = solution(i);
-    if (newBegin >= 0) {
-      i = newBegin;
-    }
+  solution(0, 0);
+
+  if (S == 0) {
+    total -= 1;  // 아무것도 선택하지 않을 확률 하나 제거
   }
 
   cout << total << endl;
