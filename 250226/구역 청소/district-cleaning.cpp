62ea7cabfd0fbf2028315c13984361a42f2cff26
Kiara <80676180+2020147542@users.noreply.github.com>
2025-02-26 23:58:53 +0900

    Title: 구역 청소, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250226/구역 청소/district-cleaning.cpp
+++ 250226/구역 청소/district-cleaning.cpp
@@ -7,16 +7,19 @@ int line[111];
 int main(){
     cin >> a >> b >> c >> d;
 
-    if(b < c || d < a){
+    if(b < c || d < a){ // 겹치지 않을 경우
         sum = (b-a) + (d-c);
     }
-    else if(d >= a){
+    else if(d >= a && b >= d){
+        // 겹치는데 순서: c a d b
         sum = (a-c) + (d-a) + (b-d);
     } 
-    else if(b >= c){
-        sum = (c-a) + (d-b) + (b-c);
+    else if(b >= c && d >= b){
+        // 겹치는데 순서: a c b d
+        sum = (c-a) + (b-c) + (d - b);
     }
 
     cout << sum;
+
     return 0;
 }
