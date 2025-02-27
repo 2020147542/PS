bd78f6d43eec7c79c2c62febfb9cf4c774d4d33a
Kiara <80676180+2020147542@users.noreply.github.com>
2025-02-27 17:58:58 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250227/코딩톡/coding-talk.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, p, cnt;
+bool sus[26];
+char c[100];
+int u[100];
+
+int main() {
+    cin >> n >> m >> p;
+
+    for (int i = 0; i < m; i++) {
+        cin >> c[i] >> u[i];
+    }
+
+    for(int i = m - 1; i >= p - 1; i--){
+        if(!sus[c[i] - 'A']){
+            sus[c[i] - 'A'] = true;
+            cnt++;
+        }  
+    }
+
+    if(n - cnt > u[p - 1]){
+        for(int i = p - 2; i >= 0; i--){
+            if(u[i] != u[p-1]) break;
+            
+            sus[c[i] - 'A'] = true;
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        if(!sus[i]){
+            cout << (char)(i + 'A') << " ";
+        }
+    }
+
+    return 0;
+}
