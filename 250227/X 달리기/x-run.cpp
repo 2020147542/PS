70e2516655b6a21a77199c4206206ee2e3e22bcc
Kiara <80676180+2020147542@users.noreply.github.com>
2025-02-27 18:41:57 +0900

    Title: X 달리기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250227/X 달리기/x-run.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int x, cnt = 1;
+int backDist[1111];
+int speed = 1, dist = 1;
+bool timeToStop;
+
+int main(){
+
+    cin >> x;
+    
+    for(int i = 1; i <= x; i++){
+        backDist[i] = backDist[i-1] + i;
+    }
+
+    while(dist < x){ // 1초에 한번씩 확인
+
+        if(backDist[speed + 1] <= x - dist){
+            speed++;
+        } else if(speed >= 2){
+            speed--;
+        }
+
+        dist += speed;
+        cnt++;
+    }
+
+    cout << cnt;
+    
+    return 0;
+}
