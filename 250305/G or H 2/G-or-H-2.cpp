71e0ec6f77e7c1b3585a4b6ab9798e7a50fed495
Kiara <80676180+2020147542@users.noreply.github.com>
2025-03-05 23:07:47 +0900

    Title: G or H 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250305/G or H 2/G-or-H-2.cpp
+++ 250305/G or H 2/G-or-H-2.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <climits>
+#include <algorithm>
 using namespace std;
 
 int n, ans = INT_MIN;
+int people[111];
 int cnt_g[111];
 int cnt_h[111];
 
 int main() {
     // Please write your code here.
-
     cin >> n;
     if(n == 1) {
         cout << 0;
@@ -16,30 +17,32 @@ int main() {
     }
 
     for(int i = 0; i < n; i++){
-        int x;
         char y;
-        cin >> x >> y;
+        cin >> people[i] >> y;
 
-        if(y == 'G') cnt_g[x] = 1;
-        else cnt_h[x] = 1;
+        if(y == 'G') cnt_g[people[i]] = 1;
+        else cnt_h[people[i]] = 1;
     }
+
+    sort(people, people + n);
 
     // 모든 경우에
     // 가능한 모든 범위에서
-    for(int i = 0; i <= 100; i++){
-        if(cnt_g[i] || cnt_h[i]){
-            
-            // 할걸 하자
-            // g와 h가 정확하게 같은 개수가 나오는 경우를 구해라
-            int g = 0;
-            int h = 0;
-            for(int j = i; j <= 100; j++){
-                g += cnt_g[j];
-                h += cnt_h[j];
+    for(int i = 0; i < n; i++){
+        int start = people[i];
 
-                if((cnt_g[j] || cnt_h[j]) && g == h){
-                    ans = max(ans, j - i);
-                }
+        // 할걸 하자
+        // g와 h가 정확하게 같은 개수가 나오는 경우를 구해라
+        int g = 0;
+        int h = 0;
+        for(int j = i; j < n; j++){
+            int end = people[j];
+            
+            g += cnt_g[end];
+            h += cnt_h[end];
+
+            if(g == h){
+                ans = max(ans, end - start);
             }
         }
     }
