60d994bcb553593768f2ae4affa90bf835a88bee
Kiara <80676180+2020147542@users.noreply.github.com>
2025-03-05 17:36:28 +0900

    Title: 겹치지 않게 선분 고르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250305/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 250305/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 #include <vector>
 using namespace std;
 typedef pair<int, int> pir;
 
 int n, ans;
+int visited[1111];
 pir lines[20];
 vector<pir> arr;
 
 void choose(int idx){
     if(idx >= n){
-        sort(arr.begin(), arr.end());
-
-        for(int i = 1; i < arr.size(); i++){
-            if(arr[i-1].second >= arr[i].first) return;
+        memset(visited, 0, sizeof(visited));
+        
+        for(int i = 0; i < arr.size(); i++){
+            for(int j = arr[i].first; j <= arr[i].second; j++){
+                if(visited[j]) return;
+                visited[j]++;
+            }
         }
 
         ans = max(ans, (int)arr.size());
