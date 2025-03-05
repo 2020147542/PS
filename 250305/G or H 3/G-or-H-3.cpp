23ccfd26ae439a886589f40bdc65cb5a347cbe53
Kiara <80676180+2020147542@users.noreply.github.com>
2025-03-05 20:18:49 +0900

    Title: G or H 3, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 250305/G or H 3/G-or-H-3.cpp
+++ 250305/G or H 3/G-or-H-3.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int n, k;
 char arr[11111];
+int cnt[11111];
 int ans, max_pos = 10000;
 
 int main() {
@@ -13,18 +14,21 @@ int main() {
     for(int i = 0; i < n; i++){
         int pos;
         cin >> pos >> arr[pos];
+        cnt[pos] += arr[pos] == 'G' ? 1 : 2;
     }
 
     for(int i = 1; i <= max_pos - k; i++){
-        int cnt = 0;
+        int sum = 0;
         for(int j = i; j <= i + k; j++){
-            if(arr[j] == 'H') cnt += 2;
-            else if(arr[j] == 'G') cnt++;
+            // if(arr[j] == 'H') sum += 2;
+            // else if(arr[j] == 'G') sum++;
+            sum += cnt[j];
         }
 
         // 할거 한다.
         // 최대 점수를 구한다.
-        ans = max(ans, cnt);
+        // ans = max(ans, cnt);
+        ans = max(ans, sum);
     }
 
     cout << ans;
