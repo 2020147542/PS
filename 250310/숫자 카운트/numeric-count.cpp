b88a97c1490cf4f15dea6c6953a8e78f11b90f95
Kiara <80676180+2020147542@users.noreply.github.com>
2025-03-10 15:39:29 +0900

    Title: 숫자 카운트, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250310/숫자 카운트/numeric-count.cpp
+++ 250310/숫자 카운트/numeric-count.cpp
@@ -1,52 +1,54 @@
 #include <iostream>
-#include <set>
+#include <unordered_set>
+#include <vector>
 using namespace std;
 
-int n;
-set<int> a;
+vector<int> a;
 
 void find(int b, int cnt1, int cnt2){
-
-    // cnt1 = b의 3자리 숫자들중 a와 동일한 위치에 위치한 경우
-    // cnt2 = b의 3자리 숫자들중 a에 존재하지만 다른 위치에 위치한 경우
-
     int digit[3] = {b/100, (b/10)%10, b% 10};
-    int c1 = 0, c2 = 0;
+    unordered_set<int> now = {b/100, (b/10)%10, b% 10};
+    vector<int> remain;
 
-    for(int i = 1; i <= 9; i++){
-        for(int j = 1; j <= 9; j++){
-            if(i == j) continue;
+    for(int num : a){
+        int i = num / 100;
+        int j = (num / 10) % 10;
+        int k = num % 10;
 
-            for(int k = 1; k <= 9; k++){
-                if(j == k) continue;
+        int c1 = 0, c2 = 0;
+        if(i == digit[0]) c1++;
+        if(j == digit[1]) c1++;
+        if(k == digit[2]) c1++;
 
-                for(int idx = 0; idx < 3; idx++){
-                    if(i == digit[idx]){
-                        if(idx == 0) c1++;
-                        else c2++;
-                    }
-                    if(j == digit[idx]){
-                        if(idx == 0) c1++;
-                        else c2++;
-                    }
-                    if(k == digit[idx]){
-                        if(idx == 0) c1++;
-                        else c2++;
-                    }
-                }
-
-                if(c1 == cnt1 && c2 == cnt2) {
-                   a.insert(i*100 + j *10 + k);
-                }
-            }
+        if(i != digit[0] && now.count(i)) c2++;
+        if(j != digit[1] && now.count(j)) c2++;
+        if(k != digit[2] && now.count(k)) c2++;
+        
+        if(c1 == cnt1 && c2 == cnt2){
+            remain.push_back(num);
         }
     }
+
+    a = remain;
 }
 
 int main() {
 
+    int n;
     cin >> n;
-    for(int i = 0; i < n; i++){
+
+    for(int i = 1; i <= 9; i++){
+        for(int j = 1; j <= 9; j++){
+            if(i == j) continue;
+            for(int k = 1; k <= 9; k++){
+                if(i == k || j == k) continue;
+
+                a.push_back(i * 100 + j * 10 + k);
+            }
+        }
+    }
+
+    for(int i = 1; i <= n; i++){
         int b, cnt1, cnt2;
         cin >> b >> cnt1 >> cnt2;
 
