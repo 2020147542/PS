391eb1d01e05fadcacdc683bc75078dd7fb0c5c5
Kiara <80676180+2020147542@users.noreply.github.com>
2025-03-11 00:27:09 +0900

    Title: 알파벳과 사칙연산, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250311/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+using namespace std;
+
+string line;
+vector<vector<int>> numbers;
+int ans = INT_MIN, cnt;
+
+int main() {
+    // Please write your code here.
+
+    for(int a = 1; a <= 4; a++){
+        for(int b = 1; b <= 4; b++){
+            for(int c = 1; c <= 4; c++){
+                for(int d = 1; d <= 4; d++){
+                    for(int e = 1; e <= 4; e++){
+                        for(int f = 1; f <= 4; f++){
+                            numbers.push_back({a, b, c, d, e, f});
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    cin >> line;
+    for(int j = 0; j < numbers.size(); j++){
+        int res = numbers[j][line[0]-'a'];
+
+        for(int i = 1; i < line.size() - 1; i++){
+    
+            if(line[i] == '*'){
+                res *= numbers[j][line[i+1]-'a'];
+            }else if(line[i] == '-'){
+                res -= numbers[j][line[i+1]-'a'];
+            }else if(line[i] == '+'){
+                res += numbers[j][line[i+1]-'a'];
+            }
+
+            ans = max(ans, res);
+        }
+    }
+
+    cout << ans;
+    
+    return 0;
+}
