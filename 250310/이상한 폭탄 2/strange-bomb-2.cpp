2a71e2f8a00e13a8e9d3d818cb65d12953754097
Kiara <80676180+2020147542@users.noreply.github.com>
2025-03-10 16:42:52 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250310/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int n, k, max_idx = INT_MIN;
+int bombs[111];
+
+int main() {
+    // Please write your code here.
+    cin >> n >> k;
+    for(int i = 0; i < n; i++){
+        cin >> bombs[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = i - k; j <= i + k; j++){
+            if(j < 0 || i == j) continue;
+            if(bombs[i] == bombs[j]){
+                max_idx = max(max_idx, bombs[j]);
+            }
+        }
+    }
+
+    if(max_idx == INT_MAX){
+        cout << -1;
+    } else{
+        cout << max_idx;
+    }
+
+    return 0;
+}
