562b83b18976115c7e3dd93a024f6289c3213d18
Kiara <80676180+2020147542@users.noreply.github.com>
2025-03-10 17:17:21 +0900

    Title: 흥미로운 숫자 2, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

--- 250310/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 250310/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -7,22 +7,20 @@ int x, y;
 vector<int> arr;
 
 bool isInteresting(int num){
-    vector<int> digits;
+    vector<int> digits(10, 0);
     
+    int n = 0;
     while(num){
         int now = num % 10;
-        digits.push_back(now);
+        digits[now]++;
+        n++;
         num /= 10;
     }
     
-    int diff = 0;
-    int n = digits.size();
-    for(int i = 0; i < n; i++){
-        int cnt = count(digits.begin(), digits.end(), digits[i]);
-        if(cnt != n-1) diff++;
+    for(int i = 0; i < 10; i++){
+        if(digits[i] == n-1) return true;
     }
-
-    if(diff == 1) return true;
+    
     return false;
 }
 
